695a0c52287767dbe01a0da73e01118a9475b4ce
규일 <113951681+tlgms@users.noreply.github.com>
2026-04-09 14:38:59 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 124 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(
+                (n+1)*2 + " " + (n+1)*3
+        ));
+
+        bw.flush();
+        bw.close();
+    }
+}
