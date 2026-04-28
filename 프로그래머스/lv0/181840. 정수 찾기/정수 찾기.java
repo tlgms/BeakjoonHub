c44cb3fdc91407581d3daafbbb1c52cd02d754ba
규일 <113951681+tlgms@users.noreply.github.com>
2026-04-28 13:38:52 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+
+        for (int num : num_list) {
+            if (num == n) {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
